23232d7337a2d3bb0b85ad63cccba45315d61997
kosmos <kosmos.by@gmail.com>
2015-07-14 16:27:53 +0300

    removed 2 another useless files

--- mail.php
+++
@@ -1,9 +0,0 @@
-<?php
-$to = "lapytko.spb@mail.ru";
-$subject = "Test mail";
-$message = "Hello! This is a simple email message.";
-$from = "lapytko-yura@ya.ru";
-$headers = "From:" . $from;
-echo mail($to,$subject,$message,$headers)."<hr>";
-echo "Mail Sent.";
-?> 
